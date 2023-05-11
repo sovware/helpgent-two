ccbb5c166cd664bf11eb963f72b5958cf7f6a4b9
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-05-11 12:00:53 +0600

    improved

--- app/MediaDriver/Local.php
+++ app/MediaDriver/Local.php
@@ -7,7 +7,6 @@ use HelpGent\App\Contracts\MediaDriver;
 use HelpGent\App\DTO\AttachmentFileDTO;
 
 class Local implements MediaDriver {
-
     /**
      * @return AttachmentFileDTO|Exception
      */
