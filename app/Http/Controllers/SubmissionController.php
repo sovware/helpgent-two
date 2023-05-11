ca5b01d5d4733a724decc282caff7f78bffa7253
MD AL AMIN <mdalaminbey@gmail.com>
2023-05-11 14:47:15 +0600

    create submission cru

---
+++ app/Http/Controllers/SubmissionController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace HelpGent\App\Http\Controllers;
+
+use HelpGent\App\DTO\SubmissionDTO;
+use HelpGent\App\Http\Controllers\Controller;
+use HelpGent\App\Repositories\FormRepository;
+use HelpGent\App\Repositories\SubmissionRepository;
+use HelpGent\WaxFramework\RequestValidator\Validator;
+use HelpGent\WaxFramework\Routing\Response;
+use WP_REST_Request;
+
+class SubmissionController extends Controller {
+    public SubmissionRepository $submission_repository;
+
+    public FormRepository $form_repository;
+
+    public function __construct( SubmissionRepository $submission_repository, FormRepository $form_repository ) {
+        $this->submission_repository = $submission_repository;
+        $this->form_repository       = $form_repository;
+    }
+
+    public function index() {
+        return Response::send(
+            [
+                'submission' => $this->submission_repository->get()
+            ]
+        );
+    }
+
+    public function store( Validator $validator, WP_REST_Request $wp_rest_request ) {
+        $validator->validate(
+            [
+                'form_id' => 'required|numeric'
+            ]
+        );
+
+        if ( $validator->is_fail() ) {
+            return Response::send(
+                [
+                    'messages' => $validator->errors
+                ], 422
+            );
+        }
+
+        $submission_dto = new SubmissionDTO(
+            $wp_rest_request->get_param( 'form_id' ),
+            get_current_user_id()
+        );
+
+        $submission_id = $this->submission_repository->create( $submission_dto );
+
+        return Response::send(
+            [
+                'submission_id' => $submission_id,
+                'message'       => esc_html__( 'Submission Created Successfully!', 'helpgent' )
+            ]
+        );
+    }
+}
---
+++ app/Models/Submission.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace HelpGent\App\Models;
+
+use HelpGent\WaxFramework\App;
+use HelpGent\WaxFramework\Database\Resolver;
+use HelpGent\WaxFramework\Database\Eloquent\Model;
+
+class Submission extends Model {
+    public static function get_table_name():string {
+        return 'helpgent_submissions';
+    }
+
+    public function resolver():Resolver {
+        return App::$container->get( Resolver::class );
+    }
+}
