08f68fc31d1afc458414f2f8f70c636aa757172d
MD AL AMIN <75071900+mdalaminbey@users.noreply.github.com>
2023-05-11 11:03:16 +0600

    Update api.php

--- routes/rest/api.php
+++ routes/rest/api.php
@@ -3,6 +3,7 @@
 use HelpGent\App\Http\Controllers\Admin\FormController;
 use HelpGent\WaxFramework\Routing\Route;
 
+
 Route::group(
     'admin', function() {
         Route::resource( 'form', FormController::class );
