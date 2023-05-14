a7e6226495b504ea776a515aff2765c658ce2eaa
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-05-14 16:33:48 +0600

    improved

--- app/Contracts/MediaDriver.php
+++ app/Contracts/MediaDriver.php
@@ -6,10 +6,8 @@ use Exception;
 use HelpGent\App\DTO\AttachmentFileDTO;
 
 interface MediaDriver {
+
     /**
-     * Upload
-     * 
-     * @param array $file
      * @return AttachmentFileDTO|Exception
      */
     public function upload( array $file );
@@ -17,4 +15,5 @@ interface MediaDriver {
     public function get( array $attachment ) : AttachmentFileDTO;
 
     public function delete( AttachmentFileDTO $attachment ) : bool;
+
 }
