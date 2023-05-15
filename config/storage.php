d2d3ea6bb59b9baca236336bc93108e1e96f65f5
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-05-15 10:45:18 +0600

    improved

--- config/storage.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined( 'ABSPATH' ) || exit;
-
-return [
-    'upload_dir_path' => WP_CONTENT_DIR . '/uploads/helpgent',
-    'upload_dir_url'  => WP_CONTENT_URL . '/uploads/helpgent',
-];
