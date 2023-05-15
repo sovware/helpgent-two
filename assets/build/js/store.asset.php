6e4e449fd2e08f21e080e724ecfd812f63b2a5fc
tanjimhasan <tanjim.hasan.pro@gmail.com>
2023-05-15 15:22:58 +0600

    regiterModule issue fixed

--- assets/build/js/store.asset.php
+++ assets/build/js/store.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data'), 'version' => 'cd24159957119909f33d');
+<?php return array('dependencies' => array('wp-data'), 'version' => '5f085f9f6e0dbcb976be');
