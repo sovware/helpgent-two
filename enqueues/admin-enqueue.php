26777e45280ce9d6425d0966ed96151af831afe2
MD AL AMIN <mdalaminbey@gmail.com>
2023-05-16 10:42:42 +0600

    Update admin-enqueue.php

--- enqueues/admin-enqueue.php
+++ enqueues/admin-enqueue.php
@@ -6,5 +6,7 @@ defined( 'ABSPATH' ) || exit;
 
 Enqueue::register_script( 'helpgent/store', 'build/js/store' );
 
-Enqueue::script( 'wax-app-script', 'build/js/app' );
+if ( 'helpgent_page_helpgent' === get_current_screen()->id ) {
+    Enqueue::script( 'helpgent-admin-app', 'build/js/app' );
+}
 // Enqueue::style( 'wax-app-style', 'build/css/app' );
