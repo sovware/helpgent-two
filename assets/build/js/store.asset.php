6e5185e9cd373fb64134f4c01dae48c83a58f5bd
tanjimhasan <tanjim.hasan.pro@gmail.com>
2023-05-17 12:46:26 +0600

    update

--- assets/build/js/store.asset.php
+++ assets/build/js/store.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data'), 'version' => '5f085f9f6e0dbcb976be');
+<?php return array('dependencies' => array('wp-data'), 'version' => 'd123ab1491cdcda5ba6c');
