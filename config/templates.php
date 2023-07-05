e29f5d3aabf9d1cd3da35477442cfa872ec799bc
MD AL AMIN <mdalaminbey@gmail.com>
2023-07-05 17:46:12 +0600

    create premade template basement

---
+++ config/templates.php
@@ -0,0 +1,11 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+return [
+    'sample_one' => [
+        'title'    => esc_html__( 'Sample One', 'helpgent' ),
+        'is_pro'   => false,
+        'category' => 'survey'
+    ]
+];
