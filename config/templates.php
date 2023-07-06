b296fb0e34841479e202653e652c2d4dd9e07f03
MD AL AMIN <mdalaminbey@gmail.com>
2023-07-06 10:09:43 +0600

    update pre-made template

--- config/templates.php
+++ config/templates.php
@@ -7,5 +7,10 @@ return [
         'title'    => esc_html__( 'Sample One', 'helpgent' ),
         'is_pro'   => false,
         'category' => 'survey'
-    ]
+    ],
+    'sample_two' => [
+        'title'    => esc_html__( 'Sample Two', 'helpgent' ),
+        'is_pro'   => true,
+        'category' => 'quiz'
+    ],
 ];
